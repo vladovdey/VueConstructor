db4585e454db58e5468a6191a3386307d6019753
vladovdey <ovdevlad@gmail.com>
2019-06-26 16:01:49 +0300

    added header

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="container-fluid">
+    <div class="col-12 text-center">
+      <h1>Make your own Pizza</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+</style>
